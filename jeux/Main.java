589f540a2a50378b5fcf99c630f9c89e17c98dd1
Aurélien Bocquet <gitlab@gobountz.fr>
2025-12-10 22:06:19 +0100

    Co-authored-by: jenormus <jenormus@users.noreply.github.com> Co-authored-by: Gobountz <Gobountz@users.noreply.github.com>

---
+++ jeux/Main.java
@@ -0,0 +1,15 @@
+class Main extends Program{
+
+    void algorithm(){
+        print(loadSprite("entité/Ascii art pnj/elf;txt"));
+    }
+
+    String loadSprite(String chemin){
+        String resultat="";
+        File current=newFile(chemin);
+        while(ready(current)){
+            resultat=resultat+readLine(current)+"\n";
+        }
+        return resultat;
+    }
+}
